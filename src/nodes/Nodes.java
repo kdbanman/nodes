88f07720092bdda0b294f604d2f092bd4b674295
kdbanman <kdbanman@ualberta.ca>
2013-06-06 18:21:25 -0600

    begin netbeans project transfer.

---
+++ src/nodes/Nodes.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nodes;
+
+/**
+ *
+ * @author kdbanman
+ */
+public class Nodes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
