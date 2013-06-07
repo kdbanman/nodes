78735bbd8487e9a42a870e099e4465ad6b5453ff
kdbanman <kdbanman@ualberta.ca>
2013-06-06 18:52:04 -0600

    Define necessary (speculative) classes for java implementation of full-featured version.

--- src/nodes/Nodes.java
+++ src/nodes/Nodes.java
@@ -4,16 +4,38 @@
  */
 package nodes;
 
+import processing.core.*;
+
 /**
  *
  * @author kdbanman
  */
-public class Nodes {
+public class Nodes extends PApplet {
+    
+    
+    /*
+     * Main components not written as separate class within package:
+     * - Program state tree (may not be necessary if ControlP5 is reasonably queryable
+     * - Local Jena model to store all incoming data
+     * - Subgraph cache for data mid-manipulation
+     */
+    
+    @Override
+    public void setup() {
+        size(640, 480, P3D);
+        
+    }
+    
+    @Override
+    public void draw() {
+        
+        
+    }
 
-    /**
+     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String args[]) {
+        PApplet.main(new String[]{nodes.Nodes.class.getName()});
     }
 }
