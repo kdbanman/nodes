85e01a05d33affca6b07be9f906c72386820ccde
kdbanman <kdbanman@ualberta.ca>
2013-06-24 19:05:17 -0600

    attempt label display. repackage importer.

--- src/nodes/Node.java
+++ src/nodes/Node.java
@@ -32,6 +32,9 @@ public class Node extends GraphElement<Node> {
             } else {
                 p.fill(currentCol);
             }
+            
+            displayLabel();
+            
             // Translate(x,y,0) called already, but nodes are in 3D
             p.translate(0,0,node.getPosition().z);
             p.sphere(node.size); 
