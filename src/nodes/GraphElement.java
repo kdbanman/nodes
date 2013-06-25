0979721c884d4e903c75a00b835977916aa41430
kdbanman <user@example.com>
2013-06-25 15:06:04 -0600

    try projection onto frustum for labels

--- src/nodes/GraphElement.java
+++ src/nodes/GraphElement.java
@@ -142,23 +142,58 @@ public class GraphElement<T> extends Controller<T> {
             
             pApp.popMatrix();
             * */
-        proj.calculatePickPoints(pApp.width / 2, pApp.height / 2);
-        PVector screenIn = proj.ptEndPos.get();
-        screenIn.sub(proj.ptStartPos);
-
-        PVector up = new PVector(0,1,0);
-
-        PVector axis = screenIn.cross(up);
-
-        float angle = PVector.angleBetween(screenIn, up);
+        // normalized camera direction vector 
+        PVector screenIn = proj.getDir(pApp.width / 2, pApp.height / 2);
+        screenIn.normalize();
+        
+        PVector screenCenter = proj.ptStartPos.get();
+        
+        // project reference horizontal (1,0,0) onto screen plane (near frustum)
+            //The projection of a point q = (x, y, z) onto a plane given by a point p = (a, b, c) and a normal n = (d, e, f) is
+            //q_proj = q - dot(q - p, n) * n
+            //This calculation assumes that n is a unit vector.
+        PVector horiz = new PVector(100,0,0);
+        float projectionCoeff = PVector.sub(horiz, screenCenter).dot(screenIn);
+        PVector projectedHoriz = PVector.sub(horiz, PVector.mult(screenIn, projectionCoeff));
+        
+        PVector origin = new PVector(0,0,0);
+        projectionCoeff = PVector.sub(origin, screenCenter).dot(screenIn);
+        PVector projectedOrigin = PVector.sub(origin, PVector.mult(screenIn, projectionCoeff));
+        
+        projectedHoriz.sub(screenCenter);
+        projectedOrigin.sub(screenCenter);
+        projectedHoriz.sub(projectedOrigin);
+        
+        //DEBUG
+        pApp.stroke(0);
+        pApp.line(0,0,0, projectedHoriz.x, projectedHoriz.y, projectedHoriz.z);
+        pApp.line(0,0,0, projectedOrigin.x, projectedOrigin.y, projectedOrigin.z);
+        pApp.noStroke();
+        
+        // get angle of rotation to keep text vertically aligned
+        proj.calculatePickPoints(pApp.width, pApp.height / 2);
+        PVector screenHoriz = proj.ptStartPos.get();
+        screenHoriz.sub(screenCenter);
+        float vertAngle = PVector.angleBetween(projectedHoriz, screenHoriz);
+        //DEBUG
+        pApp.println(vertAngle);
+        
+        // get axis and angle of rotation to align orthogonally to text rendering plane
+        PVector in = new PVector(0,0,-1);
+        PVector inAxis = screenIn.cross(in);
+        float inAngle = PVector.angleBetween(screenIn, in);
 
         pApp.pushMatrix();
-        pApp.rotate(-1*angle, axis.x, axis.y, axis.z);
+        // rotate to align orthogonally to text rendering plane
+        pApp.rotate(-inAngle, inAxis.x, inAxis.y, inAxis.z);
+        // rotate to align with text rendering plane's horizontal and vertical axes
+        pApp.rotateZ(vertAngle);
 
         pApp.textSize(labelSize);
 
-        // translate() already called within display() function
-        pApp.text(labelText, 0,0,0);
+        // translate() already called within display() function, so
+        // text @ (0,0,0) + size offset for separation
+        pApp.text(labelText, size,size,size);
 
         pApp.popMatrix();
     }
